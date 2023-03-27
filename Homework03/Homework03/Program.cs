620daaa7c183b490831b261f252e1da60b3e0849
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-03-27 14:27:32 +0500

    improved

--- Homework03/Homework03/Program.cs
+++ Homework03/Homework03/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+                int num1, num2, num3, num4;
+                int smallest, largest;
+
+                Console.WriteLine("Enter four integers:");
+                num1 = int.Parse(Console.ReadLine());
+                num2 = int.Parse(Console.ReadLine());
+                num3 = int.Parse(Console.ReadLine());
+                num4 = int.Parse(Console.ReadLine());
+
+                smallest = num1;
+                largest = num1;
+
+                if (num2 < smallest) smallest = num2;
+                if (num2 > largest) largest = num2;
+
+                if (num3 < smallest) smallest = num3;
+                if (num3 > largest) largest = num3;
+
+                if (num4 < smallest) smallest = num4;
+                if (num4 > largest) largest = num4;
+
+                Console.WriteLine($"Result   {largest} , {smallest}");
+
         }
     }
 }
