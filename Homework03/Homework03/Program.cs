a774877eee180ebeae50387d9e10e70cc27808db
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-03-27 14:36:45 +0500

    task1

--- Homework03/Homework03/Program.cs
+++ Homework03/Homework03/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             
-               Console.Write("Enter a decimal number: ");
+               Console.Write("Enter a dec number: ");
                int decimalNumber = int.Parse(Console.ReadLine());
                Console.WriteLine("Binary representation: {0}", Convert.ToString(decimalNumber, 2));
 
